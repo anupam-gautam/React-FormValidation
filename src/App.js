1f55f758e0ff440bd3ec0bb1890958efac06f114
anupam-gautam <anupam.gautam46@gmail.com>
2022-07-11 23:05:43 +0545

    Form Validation Code Cleanup without passing props

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import "./App.css";
 // import { AdvancedFormDisplay } from "./components/AdvancedFormDisplay";
 import { FormDisplay } from "./components/FormDisplay";
+import { TakeInput } from "./components/TakeInput";
 
 function App() {
   return (
     <div>
+      <TakeInput></TakeInput>
       <FormDisplay></FormDisplay>
+
       {/* <AdvancedFormDisplay></AdvancedFormDisplay> */}
     </div>
   );
---
+++ src/components/TakeInput.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const TakeInput = () => {
+  return (
+    <div>TakeInput</div>
+  )
+}
